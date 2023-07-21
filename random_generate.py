8ac99e16dcea0d23c3a1172a01cc488cf3d29078
Vladimir Bosinceanu <bosinceanu.vladimir@gmail.com>
2023-07-21 14:05:46 +0300

    Added more TODOs

--- random_generate.py
+++ random_generate.py
@@ -1,3 +1,3 @@
 """Starting from a random latent space representation get a spectrogram and them use generate.py on it"""
-
+"""Also it would be great to work with raw audio just to avoid the need for rebuilding the spectrograms"""
 
