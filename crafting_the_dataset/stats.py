2e25182a7ab8468972014b9396fd55b90f5a4de7
Vladimir Bosinceanu <bosinceanu.vladimir@gmail.com>
2023-07-22 14:33:07 +0300

    Duration and sample rate fast_stats

--- crafting_the_dataset/stats.py
+++ crafting_the_dataset/stats.py
@@ -1,11 +1,13 @@
-"""Check the sample rate, mono/stereo status, length and amplitude"""
+"""Check the sample rate, mono/stereo status, duration and amplitude"""
 
 import os
 import librosa
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 AUDIO_FILES_DIR = r"c:\Dataset\kicks"
+SHOW_PLOT = False
 FFT_LIB = librosa.get_fftlib()
 
 """
@@ -29,3 +31,30 @@ for root, _, files in os.walk(AUDIO_FILES_DIR):
 files = tuple(file_cluster.values())
 stats_df = pd.DataFrame.from_records(files)
 
+unique_sample_rates = stats_df["samplerate"].unique().tolist()
+num_of_unique_sample_rates = len(unique_sample_rates)
+
+
+def statistic_print(dataframe_column_name, show_unique=False):
+    print(f"{dataframe_column_name.upper()}:")
+    print("-" * 10)
+    print("mean:", round(stats_df[dataframe_column_name].mean(), 3))
+    print("stddev:", round(stats_df[dataframe_column_name].std(), 3))
+    print("min:", round(stats_df[dataframe_column_name].min(), 3))
+    print("max:", round(stats_df[dataframe_column_name].max(), 3))
+    if show_unique:
+        print("unique value counts:", stats_df[dataframe_column_name].value_counts().to_dict())
+    print("-" * 10, "\n")
+
+
+print("\n", f" -> stats for: {AUDIO_FILES_DIR} <-", "\n")
+print("*" * 25)
+statistic_print("duration")
+statistic_print("samplerate", show_unique=True)
+print("*" * 25)
+
+if SHOW_PLOT:
+    plt.hist(stats_df['samplerate'], bins=num_of_unique_sample_rates)
+    column_name = stats_df['samplerate'].name
+    plt.title(f'Histogram of {column_name}')
+    plt.show()
