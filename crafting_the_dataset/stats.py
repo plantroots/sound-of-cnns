bf3f9dad11157ff9a73a17cedae076c09f678e7b
Vladimir Bosinceanu <bosinceanu.vladimir@gmail.com>
2023-07-24 08:25:52 +0300

    Added pick_a_random_waveform_and_display_it()

--- crafting_the_dataset/stats.py
+++ crafting_the_dataset/stats.py
@@ -1,6 +1,7 @@
 """Check the duration, sample rate, mono/stereo status and rms/loudness"""
 
 import os
+import random
 import librosa
 import numpy as np
 import pandas as pd
@@ -9,6 +10,42 @@ import matplotlib.pyplot as plt
 AUDIO_FILES_DIR = r"c:\Dataset\kicks"
 SHOW_PLOT = True
 FFT_LIB = librosa.get_fftlib()
+
+
+def pick_a_random_waveform_and_display_it(signals):
+    random_index = random.randint(0, len(signals) - 1)
+    randomly_picked_array = signals[random_index]
+    plt.figure(figsize=(10, 4))
+    librosa.display.waveshow(randomly_picked_array, sr=44100)
+    plt.title('Waveform')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Amplitude')
+    plt.tight_layout()
+    plt.show()
+
+
+def statistic_print(dataframe_column_name, show_unique=False):
+    print(f"{dataframe_column_name.upper()}:")
+    print("-" * 10)
+    print("mean:", round(stats_df[dataframe_column_name].mean(), 3))
+    print("stddev:", round(stats_df[dataframe_column_name].std(), 3))
+    print("min:", round(stats_df[dataframe_column_name].min(), 3))
+    print("max:", round(stats_df[dataframe_column_name].max(), 3))
+    if show_unique:
+        print("unique value counts:", stats_df[dataframe_column_name].value_counts().to_dict())
+    print("-" * 10, "\n")
+
+
+def histogram(dataframe_column):
+    column_name = dataframe_column.name
+    unique = len(dataframe_column.unique().tolist())
+    plt.style.use('grayscale')
+    plt.hist(dataframe_column, bins=unique)
+    plt.xlabel(column_name)
+    plt.ylabel('count')
+    plt.title(column_name)
+    plt.show()
+
 
 """
 Outputs and object that looks like this:
@@ -53,6 +90,7 @@ for root, _, files in os.walk(AUDIO_FILES_DIR):
 
 # FILTER SOME OF THE SIGNALS OUT (longer than 2 seconds) - 2826 total samples in the dataset
 # 1.15 with 1x stddev and 1.74 with 2x stddev
+# at 1.74 -> 76734 samples at this samplerate
 DURATION_THRESHOLD_IN_SECONDS = 1.74
 # max_length = round(max(v["duration"] for k, v in file_cluster.items()), 2)
 
@@ -79,44 +117,12 @@ for s in signals_filtered_padded:
 print(min(ds))
 print(max(ds))
 
-# visualize them
-selected_waveforms = signals_filtered_padded[6:8]
-plt.figure(figsize=(10, 4))
-librosa.display.waveshow(selected_waveforms[0], sr=44100)
-plt.title('Waveform')
-plt.xlabel('Time (seconds)')
-plt.ylabel('Amplitude')
-plt.tight_layout()
-plt.show()
-
 files = tuple(file_cluster.values())
 stats_df = pd.DataFrame.from_records(files)
 
 unique_sample_rates = len(stats_df["samplerate"].unique().tolist())
 
-
-def statistic_print(dataframe_column_name, show_unique=False):
-    print(f"{dataframe_column_name.upper()}:")
-    print("-" * 10)
-    print("mean:", round(stats_df[dataframe_column_name].mean(), 3))
-    print("stddev:", round(stats_df[dataframe_column_name].std(), 3))
-    print("min:", round(stats_df[dataframe_column_name].min(), 3))
-    print("max:", round(stats_df[dataframe_column_name].max(), 3))
-    if show_unique:
-        print("unique value counts:", stats_df[dataframe_column_name].value_counts().to_dict())
-    print("-" * 10, "\n")
-
-
-def histogram(dataframe_column):
-    column_name = dataframe_column.name
-    unique = len(dataframe_column.unique().tolist())
-    plt.style.use('grayscale')
-    plt.hist(dataframe_column, bins=unique)
-    plt.xlabel(column_name)
-    plt.ylabel('count')
-    plt.title(column_name)
-    plt.show()
-
+pick_a_random_waveform_and_display_it(signals_filtered_padded)
 
 print("\n", f" -> stats for: {AUDIO_FILES_DIR} <-", "\n")
 print("*" * 25)
