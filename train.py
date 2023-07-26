aa2e1c66f7146e285d3b50d27324ee6be5a476a8
Vladimir Bosinceanu <bosinceanu.vladimir@gmail.com>
2023-07-26 20:06:22 +0300

    Added normalization to the raw audio (idiosyncratic) while loading the data

--- train.py
+++ train.py
@@ -14,23 +14,26 @@ BATCH_SIZE = 2
 EPOCHS = 30
 
 
-def load_fsdd(spectrograms_path):
-    x_train = []
-    for root, _, file_names in os.walk(spectrograms_path):
+def load_dataset(audio_dir):
+    train_set = []
+    for root, _, file_names in os.walk(audio_dir):
         for file_name in file_names:
             file_path = os.path.join(root, file_name)
-            # these are 2D while the greyscale images where 3D (28, 28, 1)
-            # we need to add an extra dimensions to them
-            signal, _ = librosa.load(file_path, mono=True, sr=SAMPLE_RATE)  # (n_bins, n_frames)
+            signal, _ = librosa.load(file_path, mono=True, sr=SAMPLE_RATE)
 
-            if len(signal) < NUM_OF_SAMPLES_IN_A_FILE:
-                padding_to_add = NUM_OF_SAMPLES_IN_A_FILE - len(signal)
-                signal = np.append(signal, np.zeros(padding_to_add))
-            x_train.append(signal)
-    x_train = np.array(x_train)
-    # reshaping for audio data
-    x_train = x_train[..., np.newaxis, np.newaxis]  # -> (2737, 76736, 1, 1)
-    return x_train
+            mean = np.mean(signal)
+            std = np.std(signal)
+            normalized_signal = (signal - mean) / std
+
+            if len(normalized_signal) < NUM_OF_SAMPLES_IN_A_FILE:
+                padding_to_add = NUM_OF_SAMPLES_IN_A_FILE - len(normalized_signal)
+                normalized_signal = np.append(normalized_signal, np.zeros(padding_to_add))
+
+            train_set.append(normalized_signal)
+
+    train_set = np.array(train_set)
+    train_set = train_set[..., np.newaxis, np.newaxis]  # -> (2737, 76736, 1, 1)
+    return train_set
 
 
 def train(x_train, learning_rate, batch_size, epochs):
@@ -49,7 +52,7 @@ def train(x_train, learning_rate, batch_size, epochs):
 
 
 if __name__ == "__main__":
-    x_train = load_fsdd(AUDIO_DIR)
+    x_train = load_dataset(AUDIO_DIR)
     autoencoder = train(x_train, LEARNING_RATE, BATCH_SIZE, EPOCHS)
     autoencoder.save("model")
     # autoencoder2 = Autoencoder.load("model")
