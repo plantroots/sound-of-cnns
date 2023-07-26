7253b0ae4789f9aa7c0fe8fd966f9788d9e3a9f7
Vladimir Bosinceanu <bosinceanu.vladimir@gmail.com>
2023-07-27 02:14:04 +0300

    Added mean_and_stddev_of_the_train_dataset save to disk

--- latent_space_sampling.py
+++ latent_space_sampling.py
@@ -1,11 +1,20 @@
+import os
+import pickle
+import soundfile
 import numpy as np
 from vae import VAE
 
 variational_autoencoder = VAE.load(save_folder=r"c:\Code\sound-of-cnns\model")
 variational_autoencoder.summary()
 
+DENORMALIZATION_PARAMETERS_PATH = r"C:\Code\sound-of-cnns\crafting_the_dataset\metadata\mean_and_stddev.pkl"
+OUTPUT_PATH = "samples"
 
-def sample_from_latent_space_and_feed_to_decoder(vae_model, num_of_samples=1):
+with open(DENORMALIZATION_PARAMETERS_PATH, "rb") as f:
+    denormalization_parameters = pickle.load(f)
+
+
+def sample_from_latent_space_and_feed_to_decoder(vae_model, denormalize_params, num_of_samples=1):
     # Get the dimensions of the latent space
     latent_dim = vae_model.latent_space_dim
 
@@ -15,8 +24,21 @@ def sample_from_latent_space_and_feed_to_decoder(vae_model, num_of_samples=1):
     # Use the VAEs decoder to generate new data points from the sampled latent vectors
     generated_data = vae_model.decoder.predict(sampled_latent_vectors)
 
-    return generated_data
+    # Denormalize
+    mean = denormalize_params["mean"]
+    stddev = denormalize_params["stddev"]
+
+    signal = generated_data * stddev + mean
+    return generated_data, signal
 
 
 # 'generated_samples' contains the randomly generated data points in the original data space.
-generated_samples = sample_from_latent_space_and_feed_to_decoder(vae_model=variational_autoencoder, num_of_samples=1)
+generated_samples, signal_samples = sample_from_latent_space_and_feed_to_decoder(vae_model=variational_autoencoder,
+                                                                                 denormalize_params=denormalization_parameters,
+                                                                                 num_of_samples=1
+                                                                                 )
+# TODO: FIX THE OUTPUT DIMENSION SO THAT IT WILL MATCH THE INPUT
+# Reshape
+reshaped = signal_samples[0].reshape((613888,))
+# Save the generated data
+soundfile.write(os.path.join(OUTPUT_PATH, "sample_1.wav"), reshaped, 22050)
--- train.py
+++ train.py
@@ -1,10 +1,12 @@
 import os
+import pickle
 import librosa
 import numpy as np
 
 from vae import VAE
 
 AUDIO_DIR = r"c:\Dataset\filtered_kicks_small"
+METADATA_DIR = r"C:\Code\sound-of-cnns\crafting_the_dataset\metadata"
 # 22050/44100 -> 38368/76736
 SAMPLE_RATE = 22050
 NUM_OF_SAMPLES_IN_A_FILE = 38368  # 76736 instead of 76734 so that the graph works
@@ -16,6 +18,8 @@ EPOCHS = 1000
 
 def load_dataset(audio_dir):
     train_set = []
+    means = []
+    standard_deviations = []
     for root, _, file_names in os.walk(audio_dir):
         for file_name in file_names:
             file_path = os.path.join(root, file_name)
@@ -23,6 +27,10 @@ def load_dataset(audio_dir):
 
             mean = np.mean(signal)
             std = np.std(signal)
+
+            means.append(mean)
+            standard_deviations.append(std)
+
             normalized_signal = (signal - mean) / std
 
             if len(normalized_signal) < NUM_OF_SAMPLES_IN_A_FILE:
@@ -33,7 +41,24 @@ def load_dataset(audio_dir):
 
     train_set = np.array(train_set)
     train_set = train_set[..., np.newaxis, np.newaxis]  # -> (2737, 76736, 1, 1)
+
+    mean_and_stddev_of_the_train_dataset = {
+        "mean": average_list_elements(means),
+        "stddev": average_list_elements(standard_deviations)
+    }
+
+    with open(os.path.join(METADATA_DIR, "mean_and_stddev.pkl"), "wb") as f:
+        pickle.dump(mean_and_stddev_of_the_train_dataset, f)
+
     return train_set
+
+
+def average_list_elements(input_list):
+    if not input_list:
+        return None
+    total_sum = sum(input_list)
+    average = total_sum / len(input_list)
+    return average
 
 
 def train(x_train, learning_rate, batch_size, epochs):
@@ -55,5 +80,3 @@ if __name__ == "__main__":
     x_train = load_dataset(AUDIO_DIR)
     autoencoder = train(x_train, LEARNING_RATE, BATCH_SIZE, EPOCHS)
     autoencoder.save("model")
-    # autoencoder2 = Autoencoder.load("model")
-    # autoencoder2.summary()
