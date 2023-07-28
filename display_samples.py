1a5aa5d802fe3de4fe4d9528cbb859dc8245982a
Vladimir Bosinceanu <bosinceanu.vladimir@gmail.com>
2023-07-28 08:51:30 +0300

    Switched to generated samples display

--- display_samples.py
+++ display_samples.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 from utils import read_audio_files
 from train import peak_amplitude_normalization
 
-# SAMPLE_RATE = 22050
-SAMPLE_RATE = 44100
+SAMPLE_RATE = 22050
+# SAMPLE_RATE = 44100
 
 # generated samples
-# AUDIO_FILES_DIR = r"c:\Code\sound-of-cnns\samples"
+AUDIO_FILES_DIR = r"c:\Code\sound-of-cnns\samples"
 
 # original samples
-AUDIO_FILES_DIR = r"c:\Dataset\filtered_kicks_small"
+# AUDIO_FILES_DIR = r"c:\Dataset\filtered_kicks_small"
 
 
 def display_waveforms(audio_dir, normalization=False):
